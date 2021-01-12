628ecb1009cdee908f36403483bc0a5e28cecab2
Jason Van Pelt <jvanpelt@gmail.com>
2021-01-12 15:18:40 -0500

    block access for more fully for users with no subscriptions

--- wp-content/themes/ICEAM/sensei/single-lesson.php
+++ wp-content/themes/ICEAM/sensei/single-lesson.php
@@ -25,6 +25,13 @@ $user_ID = get_current_user_id();
 $course_id = Sensei()->lesson->get_course_id( $post->ID );
 $wc_post_id = get_post_meta( $course_id, '_course_woocommerce_product', true );
 $need_to_resubscribe = wcs_user_has_subscription( $user_ID, $wc_post_id, 'active' ) == false;
+// if it is a SHL course and the user has an active subscription to the SHL bundle 
+// - next conditional is same logic for JYL
+if( wcs_user_has_subscription( $user_ID, 9604, 'active' ) == true && has_term( 'shanghan-lun', 'product_cat', $wc_post_id ) ){
+	$need_to_resubscribe = false;
+} else if ( wcs_user_has_subscription( $user_ID, 9607, 'active' ) == true && has_term( 'jingui-yaolue', 'product_cat', $wc_post_id ) ){
+	$need_to_resubscribe = false;
+}
 
 if ( have_posts() ) {
 	the_post();
@@ -90,16 +97,20 @@ if ( have_posts() ) {
 
 	<?php
 
-		/**
-		 * Hook inside the single lesson template after the content
-		 *
-		 * @since 1.9.0
-		 *
-		 * @param integer $lesson_id
-		 *
-		 * @hooked Sensei()->frontend->sensei_breadcrumb   - 30
-		 */
-		do_action( 'sensei_single_lesson_content_inside_after', get_the_ID() );
+		if( !$need_to_resubscribe ){
+
+			/**
+			 * Hook inside the single lesson template after the content
+			 *
+			 * @since 1.9.0
+			 *
+			 * @param integer $lesson_id
+			 *
+			 * @hooked Sensei()->frontend->sensei_breadcrumb   - 30
+			 */
+			do_action( 'sensei_single_lesson_content_inside_after', get_the_ID() );
+
+		}
 
 	?>
 
